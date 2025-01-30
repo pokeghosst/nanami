0b3bad3156f6bf49ac88c44429ad2030029c77aa
pokeghost <pokeghost@disroot.org>
2025-01-30 19:02:30 +0200

    Parsing case blocks

--- lib/nanami/parser.rb
+++ lib/nanami/parser.rb
@@ -1,14 +1,49 @@
+# nanami -- Nano Markdown compiler
+# Copyright (C) 2025 pokeghost
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 # frozen_string_literal: true
 
 require 'parslet'
+require 'parslet/convenience'
 
 module Nanami
   class Parser < Parslet::Parser
     rule(:space) { match('\s').repeat(1) }
     rule(:space?) { space.maybe }
     rule(:newline) { match('\n') }
+    rule(:lbrace) { str('{') }
+    rule(:rbrace) { str('}') }
 
-    rule(:title) { str('title:') >> space? >> match('[^\n]').repeat(1).as(:title) >> newline }
+    rule(:title) do
+      str('title:') >> space? >> (newline.absent? >> any).repeat(1).as(:title) >> newline
+    end
+
+    rule(:text_block) do
+      str('text') >> space? >> lbrace >> space? >>
+        (str('}').absent? >> any).repeat(1).as(:text) >>
+        space? >> rbrace
+    end
+
+    rule(:case_statement) do
+      str('case(') >>
+        (str(')').absent? >> any).repeat(1).as(:condition) >>
+        str(')') >> space? >> lbrace >> space? >>
+        (text_block | case_statement).as(:case_body) >>
+        space? >> rbrace
+    end
 
     # rule(:content) { str('content') >> space? >> str('{') >> newline | space? >>  case_block.as(:content) >> newline | space? >> str('}') }
 
--- spec/nanami_spec.rb
+++ spec/nanami_spec.rb
@@ -11,28 +11,47 @@ RSpec.describe Nanami do
   end
 
   describe 'title' do
-    it 'parses regular title' do
+    it 'can parse well-formed title' do
       input = "title: test\n"
       expect(parser.title).to parse(input)
       result = parser.title.parse(input)
       expect(result[:title].to_s.strip).to eq('test')
     end
 
-    it 'parses title with multiple spaces' do
+    it 'can parse title with multiple spaces' do
       input = "title:  test  \n"
       expect(parser.title).to parse(input)
       result = parser.title.parse(input)
       expect(result[:title].to_s.strip).to eq('test')
     end
 
-    it 'fails on invalid title format' do
+    it 'fails on malformed title' do
       expect(parser.title).not_to parse("titleTitle\n")
       expect(parser.title).not_to parse("title: \n")
       expect(parser.title).not_to parse('title: test')
     end
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe 'text' do
+    it 'can parse well-formed text block' do
+      input = "text {
+				I exist!
+			}"
+      expect(parser.text_block).to parse(input)
+      result = parser.text_block.parse(input)
+      expect(result[:text].to_s.strip).to eq('I exist!')
+    end
+  end
+
+  describe 'case' do
+    it 'can parse well-formed case block' do
+      input = "case(hello) {
+			text {
+				I exist!
+			}
+		  }"
+      # expect(parser.case_statement).to parse(input)
+      parser.case_statement.parse_with_debug(input)
+    end
   end
 end
