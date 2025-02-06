8a30af351f0ee3527d6900c9f21284ed5ebe61d2
pokeghost <pokeghost@disroot.org>
2025-02-06 21:11:35 +0200

    Produce HTML with transformer

--- lib/nanami/nama_transformer.rb
+++ lib/nanami/nama_transformer.rb
@@ -35,9 +35,9 @@ module Nanami
 
     # TODO: Brain not working now, handle refs properly later
     rule(ref: simple(:ref)) { ref.to_s }
-    rule(raw: simple(:text)) { text.to_s }
+    rule(raw: simple(:raw)) { raw.to_s }
     rule(title: simple(:title)) { title.to_s }
-    rule(case_name: simple(:case_name)) { 'not case name' }
+    rule(text: sequence(:text)) { text.join '' }
 
     rule(
       link: {
@@ -61,26 +61,26 @@ module Nanami
     rule(sources: sequence(:_)) { '<div class="sources"></div>' }
     rule(footnotes: simple(:_)) { '<div class="footnotes"></div>' }
 
-    rule(content: sequence(:cases)) do
-      print("\n\nprocessing case NOW\n\n")
-      processed_cases = cases.map do |case_data|
-        "<div class=\"case\" data-name=\"#{case_data[:case_name]}\"#{
-          case_data[:case_url] ? " data-url=\"#{case_data[:case_url]}\"" : ''
-        }>"
-      end
-
-      "<div class=\"content\">#{processed_cases.join}</div>"
-    end
-
-    # TODO: Don't forget to handle nlp
     rule(
-      title: simple(:title),
-      content: sequence(:content)
+      case_name: simple(:name),
+      case_body: sequence(:body)
     ) do
-      print("\n\nprocessing DOCUMENT\n\n")
-      # format(HTML5_TEMPLATE,
-      #        title: title,
-      #        content: content)
+      "<div class=\"case\" data-name=\"#{name}\">#{body.join ''}</div>"
     end
+
+    rule(title: simple(:title), content: sequence(:cases)) do
+      format(HTML5_TEMPLATE,
+             title: title,
+             content: "<div class=\"content\">#{cases.join ''}</div>")
+    end
+
+    # # TODO: Don't forget to handle nlp
+    # rule(
+    #   title: simple(:title),
+    #   content: sequence(:content)
+    # ) do
+    #   print("\n\nprocessing DOCUMENT\n\n")
+
+    # end
   end
 end
